b4554e1992475d4f86e501920c7665c137123f71
Bruno Oliveira de Paula <brunohdbass@gmail.com>
2026-05-28 20:10:22 -0300

    feat: wire naming-convention rule into bopstack plugin export

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { Plugin } from 'effect-oxlint';
 
-import { no_inline_styles } from './no_inline_styles.ts';
-import { no_hardcoded_colors } from './no_hardcoded_colors.ts';
-import { test_naming } from './test_naming.ts';
+import { naming_convention } from './naming_convention.ts';
 import { no_empty_catch } from './no_empty_catch.ts';
+import { no_hardcoded_colors } from './no_hardcoded_colors.ts';
+import { no_inline_styles } from './no_inline_styles.ts';
 import { no_ts_ignore } from './no_ts_ignore.ts';
+import { test_naming } from './test_naming.ts';
 
 export default Plugin.define({
 	name: 'bopstack',
 	rules: {
+		'naming-convention': naming_convention,
 		'no-inline-styles': no_inline_styles,
 		'no-hardcoded-colors': no_hardcoded_colors,
 		'test-naming': test_naming,
